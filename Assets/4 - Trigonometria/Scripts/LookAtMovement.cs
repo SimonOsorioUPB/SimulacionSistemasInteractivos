defff0ce1d36814991e455a449515bb1f19c61af
SimonOsorio <soranokuroi@gmail.com>
2022-10-12 21:13:03 -0500

    Trigonometria

---
+++ Assets/4 - Trigonometria/Scripts/LookAtMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LinearMovement))]
+public class LookAtMovement : MonoBehaviour
+{
+    private LinearMovement movement;
+    private void Awake()
+    {
+        movement = GetComponent<LinearMovement>();
+    }
+    private void Update()
+    {
+        float radians = Mathf.Atan2(movement.Velocity.y, movement.Velocity.x);
+        RotateZ(radians);
+    }
+    private void RotateZ(float radians) { transform.rotation = Quaternion.Euler(0, 0, radians * Mathf.Rad2Deg); }
+}
---
+++ Assets/4 - Trigonometria/Scripts/PolarExperiments.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PolarExperiments : MonoBehaviour
+{
+    [SerializeField] float radius;
+    [SerializeField] float angleDeg;
+
+    [Header("Speed and acceleration")]
+    [SerializeField] float angularSpeed;
+    [SerializeField] float radialSpeed;
+    [SerializeField] float radialAcceleration;
+    [SerializeField] float angularAcceleration;
+
+    [Header("World")]
+    [SerializeField] Transform bolita;
+    [SerializeField] Camera camara;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Assert.IsNotNull(bolita, "Bolita reference is required");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //incrementa en angulo y radio
+        angleDeg += angularSpeed * Time.deltaTime;
+        radius += radialSpeed * Time.deltaTime;
+
+        //update the bolita position
+        bolita.position = PolarToCartesian(radius, angleDeg);
+        Debug.DrawLine(Vector3.zero, bolita.position);
+
+        //if (Mathf.Abs(bolita.position.x) >= camara.orthographicSize)
+        //{
+        //    if (Mathf.Abs(radialAcceleration) >= 5)
+        //    {
+        //        radialAcceleration = -radialAcceleration;
+        //    }
+        //    else
+        //    {
+        //        radialSpeed = -radialSpeed;
+        //    }
+        //}
+    }
+
+    private Vector3 PolarToCartesian(float radius, float angle)
+    {
+        float x = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float y = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        return new Vector3(x, y, 0);
+    }
+
+}
